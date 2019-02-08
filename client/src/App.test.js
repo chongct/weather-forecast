763963c6f5fa0ed02cdcc336af8464ac8ea16a85
chongct <chongchingtong@gmail.com>
2019-02-09 03:45:07 +0800

    fix test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
+import Location from './components/Location';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+
+it("App component renders without crashing", () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.exists()).toEqual(true);
+});
+
+it("Location component renders without crashing", () => {
+  const wrapper = shallow(<Location />);
+  console.log(wrapper);
+  expect(wrapper.exists()).toEqual(true);
+  expect(wrapper.find("Input").length).toEqual(1);
+  expect(wrapper.find("Button").length).toEqual(1);
 });
