4fcfadd4cfbe1034bf701c981c79eb86330d1730
chongct <chongchingtong@gmail.com>
2019-02-10 01:20:11 +0800

    fixing coverage

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,6 @@ const coveralls = require('gulp-coveralls');
 
 gulp.task("coverage", function() {
   console.log("running coverage");
-  return gulp.src(["server/coverage.lcov", "client/coverage/lcov.info"])
+  return gulp.src(["./server/coverage/lcov.info", "./client/coverage/lcov.info"])
     .pipe(coveralls());
 });
