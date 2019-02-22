322109f9caf5dd489e9a0c182e32c61792a66a9d
chongct <chongchingtong@gmail.com>
2019-02-22 10:37:39 +0800

    combining lcov report

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
 const gulp = require('gulp');
+const merger = require('lcov-result-merger');
 const coveralls = require('gulp-coveralls');
 
 gulp.task("coverage", function() {
   console.log("running coverage");
-  return gulp.src(["./**/lcov.info"])
+  return gulp.src(["./client/coverage/lcov.info", "./server/coverage/lcov.info"])
+    .pipe(merger())
+    .pipe(gulp.dest("./coverage/merged"))
     .pipe(coveralls());
 });
